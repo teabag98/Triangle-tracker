25d44f0a19a8fcbcc22a6a161274261a3ec2f6a4
eric <eric.kingoo2014@gmail.com>
2019-02-15 16:57:30 +0300

    add new function in javascript

--- scripts.js
+++ scripts.js
@@ -16,10 +16,10 @@ alert("the triangle is Scalene")
 else if(side1===side2&&side1!==side3||side2===side3&&side2!==side1||side1===side3&&side1!==side2&&side2+side1<side3&&side3+side2<side1&&side1+side3<side2){
    alert("the triangle is isosceles")
 }
-else if(side1!==side2&&side2!==side3&&side3!==side1&&side2+side1>side3||side3+side2>side1||side1+side3>side2){
+else if(side2+side1>side3||side3+side2>side1||side1+side3>side2){
 alert("it is not a triangle")
 }
-   
+
 
 
 }
