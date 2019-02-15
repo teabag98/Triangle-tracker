c3752091bb111fa59ab0222fc72592d798d09319
eric <eric.kingoo2014@gmail.com>
2019-02-15 16:16:00 +0300

    add new background page

--- scripts.js
+++ scripts.js
@@ -20,6 +20,12 @@ else if(side1!==side2&&side2!==side3&&side3!==side1&&side2+side1>side3||side3+si
 alert("it is not a triangle")
 }
 
+function submitForm() {
+           document.getElementById("submit").reset();
+           }
+submitForm();
+
+
 }
 
 tracker();
