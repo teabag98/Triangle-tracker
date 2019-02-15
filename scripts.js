d9bf3ad249d5c5337bce9681b3aa7906e4e2e9ef
eric <eric.kingoo2014@gmail.com>
2019-02-15 15:50:54 +0300

    add new alert function

---
+++ scripts.js
@@ -0,0 +1,25 @@
+
+function tracker(){
+  var a = document.getElementById('input-one').value;
+  var b = document.getElementById('input-two').value;
+  var c = document.getElementById('input-three').value;
+  var side1=parseInt(a);
+  var side2=parseInt(b);
+  var side3=parseInt(c);
+
+if(side1===side2&&side2===side3&&side1===side3){
+alert("the triangle is equilateral")
+}
+else if(side1!==side2&&side2!==side3&&side3!==side1){
+alert("the triangle is Scalene")
+}
+else if(side1===side2&&side1!==side3||side2===side3&&side2!==side1||side1===side3&&side1!==side2&&side2+side1<side3&&side3+side2<side1&&side1+side3<side2){
+   alert("the triangle is isosceles")
+}
+else if(side1!==side2&&side2!==side3&&side3!==side1&&side2+side1>side3||side3+side2>side1||side1+side3>side2){
+alert("it is not a triangle")
+}
+
+}
+
+tracker();
